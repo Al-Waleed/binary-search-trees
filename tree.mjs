d328f637a468bf91895bf7b48897c544f688c898
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-31 22:39:28 +0300

    Create a deleteItem function , for now just add the logic for the first case of deleting a node

--- tree.mjs
+++ tree.mjs
@@ -42,5 +42,48 @@ export default function tree(array) {
     }
   };
 
-  return { root, insert, prettyPrint };
+  const deleteItem = (value, pointer = root, parentPointer = null) => {
+    // let pointer = root;
+    // first case: the node we want to delete is a leaf node
+    if (
+      pointer.left === null &&
+      pointer.right === null &&
+      pointer.data === value
+    ) {
+      // case a:the value we want to delete is to the left of the parentPointer
+      if (parentPointer.data > value) {
+        parentPointer.left = null;
+        return;
+        // case b:the value we want to delete is to the right of the parentPointer
+      } else if (parentPointer.data < value) {
+        parentPointer.right = null;
+        return;
+      }
+    }
+
+    // if the value is smaller than the pointer's value we go left
+    if (value < pointer.data) {
+      // to keep track of the parent of the pointer
+      parentPointer = pointer;
+      pointer = pointer.left;
+    } else if (value > pointer.data) {
+      // to keep track of the parent of the pointer
+      parentPointer = pointer;
+      pointer = pointer.right;
+    }
+
+    deleteItem(value, pointer, parentPointer);
+  };
+
+  return { root, insert, prettyPrint, deleteItem };
 }
+
+const test = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
+
+const binaryTree = tree(test);
+
+binaryTree.prettyPrint();
+console.log("########################");
+binaryTree.deleteItem(2);
+console.log("########################");
+binaryTree.prettyPrint();
