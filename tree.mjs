8a65dbe07d1c98454c440f49e9e54fda61a5b30d
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-31 17:48:35 +0300

    Create an insert function that inserts a new number in the binary tree

--- tree.mjs
+++ tree.mjs
@@ -1,11 +1,33 @@
 import buildTree from "./buildTree.mjs";
 import mergeSort from "./mergeSort.mjs";
+import node from "./node.mjs";
 
 export default function tree(array) {
   // to sort and remove duplicates
   const sortedArray = mergeSort(array);
   // to build the tree using the buildTree function
   const root = buildTree(sortedArray);
+
+  const insert = (value) => {
+    let pointer = root;
+    while (pointer.data !== value) {
+      //if the current node value is bigger than the given value and the next left node is empty we stop the loop and insert a new node with the new value
+      if (pointer.data > value && pointer.left === null) {
+        pointer.left = node(value);
+        return;
+        //if the current node value is smaller than the given value and the next right node is empty we stop the loop and insert a new node with the new value
+      } else if (pointer.data < value && pointer.right === null) {
+        pointer.right = node(value);
+        return;
+      }
+      // to go to the next node based on the given value, either left if its smaller or right if its bigger
+      if (value < pointer.data) {
+        pointer = pointer.left;
+      } else if (value > pointer.data) {
+        pointer = pointer.right;
+      }
+    }
+  };
 
   return root;
 }
